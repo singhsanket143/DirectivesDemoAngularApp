d4c1ac84903ea627f540886ce3fc35c23f6bb621
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2017-12-20 11:14:40 +0530

    HostListener added for hover effect

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -9,7 +9,13 @@ export class BetterHighlightDirective implements OnInit {
   }
 
   ngOnInit() {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'aqua');
+  }
+  @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'aqua');
+  }
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'lime');
   }
 
 }
